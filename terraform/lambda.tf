44cb3d8fdfccc5255a780b011d3798e5e0821a4d
flmngwllm <flmngwllm232@gmail.com>
2025-06-07 19:37:49 -0400

    Rename IAM role for Lambda functions to improve clarity and consistency

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -11,8 +11,8 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "event_lambda" {
+  name               = "event_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -54,7 +54,7 @@ resource "aws_iam_policy" "resource_lambda_s3_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-  role       = aws_iam_role.iam_for_lambda.name
+  role       = aws_iam_role.event_lambda.name
   policy_arn = aws_iam_policy.resource_lambda_s3_policy.arn
 }
 
@@ -72,7 +72,7 @@ resource "aws_lambda_function" "create_events" {
   # path.module in the filename.
   filename      = "lambda.zip"
   function_name = "create_events_function"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.event_lambda.arn
   handler       = "create_events_handler"
 
   source_code_hash = data.archive_file.lambda_archive.output_base64sha256
@@ -86,7 +86,7 @@ resource "aws_lambda_function" "subscribe" {
   # path.module in the filename.
   filename      = "lambda.zip"
   function_name = "subscribe_function"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.event_lambda.arn
   handler       = "subscribe_handler"
 
   source_code_hash = data.archive_file.lambda_archive.output_base64sha256
