cf26719fb7791518af469e9fb2795c2eaaab9f4a
flmngwllm <flmngwllm232@gmail.com>
2025-06-09 00:22:54 -0400

    Refactor GitHub Actions workflow: improve Lambda zipping process and clean up outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,6 @@ output "api_gateway_url" {
   value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.REGION}.amazonaws.com/${aws_api_gateway_stage.Prod_stage.stage_name}"
 }
 
-
 output "s3_website_url" {
   value = "http://${aws_s3_bucket.event_announcement.bucket}.s3-website-${var.REGION}.amazonaws.com"
 }
